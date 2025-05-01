c45ea64bafcf37af41f47e258f417c72c92faab2
blueskinlizard <blueskinlizard@gmail.com>
2025-05-01 06:19:09 -0500

    Initial drawing routes added

--- apps/server/src/prisma_queries/queries.js
+++ apps/server/src/prisma_queries/queries.js
@@ -2,7 +2,7 @@ import { PrismaClient } from '../generated/prisma/index.js';
 const prisma = new PrismaClient();
 
 export const createUser = async (name, hashedPassword) => {
-    return await prisma.User.create({
+    return await prisma.user.create({
         data: {
             name: name.toLowerCase(),
             password: hashedPassword
@@ -10,16 +10,43 @@ export const createUser = async (name, hashedPassword) => {
     })
 }
 export const findUserByName = async(paramName) =>{
-    return await prisma.User.findFirst({
+    return await prisma.user.findFirst({
         where: {
             name: paramName.toLowerCase()
+        },
+        select: {
+            //Still deciding what goes here
         }
     })
 }
 export const findUserById = async(paramId) =>{
-    return await prisma.User.findUnique({
+    return await prisma.user.findUnique({
         where: {
             id: paramId
+        },
+        select: {
+            //Still deciding what goes here
+        }
+    })
+}
+export const createWhiteboard = async(paramUser, paramName) =>{
+    return await prisma.whiteboard.create({
+        data:{
+            Owner: { connect: { id: paramUser.id } },
+            name: paramName
+        },
+        select: {
+            id: true
+        }
+    })
+}
+export const findUserWhiteboards = async(paramUser) =>{
+    return await prisma.user.findFirst({
+        where: {
+            id: paramUser.id
+        },
+        select: {
+            Whiteboard: true,
         }
     })
 }
--- apps/server/src/routes/drawing_routes.js
+++ apps/server/src/routes/drawing_routes.js
@@ -5,4 +5,37 @@ const redis = require('redis')
 const cache = redis.createClient();
 const router = express.Router();
 
+router.post("/newwhiteboard", async(req, res) =>{
+    const { whiteboardName } = req.body;
+    const currentUser = req.user;
+    if(!currentUser){ return res.status(401).json({ message: "user not authenticated" })}
+    if(!whiteboardName){ return res.status(400).json({ message: "whiteboard name not found" })}
+    try{
+        const createdWhiteboard = await db.createWhiteboard(currentUser, whiteboardName);
+        //Returns selected id as specified in query
+        return res.status(200).json({
+            id: createdWhiteboard
+        })
+        //One - time thing, no need for Redis cache optimization
+    }catch(err){
+        return res.status(500).json({ message: "error in adding whiteboard to user: "+err})
+    }
+})
+router.get("/allwhiteboards", async(req, res) =>{
+    const { inputtedUser } = req.body;
+    if(!inputtedUser){return res.status(401).json({ message: "username parameter not provided"})}
+    try{
+        const fetchedUser = await db.findUserByName(inputtedUser);
+        
+        if(!fetchedUser){ return res.status(404).json({message: "user does not exist"})}
+        const fetchedWhiteboards = await db.findUserWhiteboards(inputtedUser);
+        return res.status(200).json({
+            ownedWhiteboards: fetchedWhiteboards.Whiteboard
+        })
+
+    }catch(err){
+
+    }
+})
+
 module.exports = router;
